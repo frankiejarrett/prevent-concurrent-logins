6c384f3c5e6ce3336b37903d617a4aa3240d6723
Frankie Jarrett <frankie@x-team.com>
2014-12-31 14:41:49 -0600

    Initial project setup

---
+++ prevent-concurrent-logins.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: Prevent Concurrent Logins
+ * Description: Prevents users from being logged into the same WordPress site from multiple places.
+ * Version: 0.1.0
+ * Author: Frankie Jarrett
+ * Author URI: http://frankiejarrett.com
+ * License: GPLv2+
+ * Text Domain: prevent-concurrent-logins
+ */
+
+/**
+ * Detect if the current user has concurrent sessions
+ *
+ * @return bool
+ */
+function pcl_user_has_concurrent_sessions() {
+	return ( is_user_logged_in() && count( wp_get_all_sessions() ) > 1 );
+}
+
+/**
+ * Only allow one session per user
+ *
+ * If the current user's session has been taken over by a newer
+ * session then we will destroy their session automattically and
+ * they will be redirected to the login page.
+ *
+ * @action init
+ *
+ * @return void
+ */
+function pcl_disallow_account_sharing() {
+	if ( ! pcl_user_has_concurrent_sessions() ) {
+		return;
+	}
+
+	$newest   = max( wp_list_pluck( wp_get_all_sessions(), 'login' ) );
+	$sessions = WP_Session_Tokens::get_instance( get_current_user_id() );
+	$session  = $sessions->get( wp_get_session_token() );
+
+	if ( $session['login'] === $newest ) {
+		wp_destroy_other_sessions();
+	} else {
+		wp_destroy_current_session();
+	}
+}
+add_action( 'init', 'pcl_disallow_account_sharing' );
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Prevent Concurrent Logins ===
+Contributors:      fjarrett
+Tags:              login, users, membership, security, sessions
+Requires at least: 4.1
+Tested up to:      4.1
+Stable tag:        trunk
+License:           GPLv2 or later
+License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+
+Prevents users from being logged into the same WordPress site from multiple places.
+
+== Description ==
+
+
+
+
+If for some reason you don't want to use a plugin to do this, you can also just add this hook to your theme:
+
+And as always, if you have suggestions to make this plugin better, or find problems with it, please create an issue on [GitHub](https://github.com/fjarrett/wp-prevent-concurrent-logins).
+
+== Changelog ==
+
+= 0.1.0 - December 31, 2014 =
+
+* Initial release.
+
+Props [fjarrett](https://profiles.wordpress.org/fjarrett/)
